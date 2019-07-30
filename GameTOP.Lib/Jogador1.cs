6b267dc565945cc7ff700ea60c08b923c88e56b4
magno-dourado <luiz.ti@live.com>
2019-07-29 23:11:39 -0300

    First Commit

---
+++ GameTOP.Lib/Jogador1.cs
@@ -0,0 +1,28 @@
+using GameTOP.Interface;
+
+namespace GameTOP.Lib
+{
+    public class Jogador1 : iJogador
+    {
+        public readonly string _Nome;
+        public Jogador1(string nome)
+        {
+            _Nome = nome;
+        }
+        //Chuta
+        public string Chuta()
+        {
+            return $"{_Nome} está Chutando \n";
+        }
+        //Corre
+        public string Corre()
+        {
+            return $"{_Nome} está Correndo \n";
+        }
+        //Passe
+        public string Passe()
+        {
+            return $"{_Nome} está Passando \n";
+        }
+    }
+}
---
+++ GameTOP/JogoFoda.cs
@@ -0,0 +1,27 @@
+using System;
+using GameTOP.Interface;
+
+namespace GameTOP
+{
+    public class JogoFoda{
+        private readonly iJogador _jogador1;
+        private readonly iJogador _jogador2;
+        public JogoFoda(iJogador jogador1, iJogador jogador2)
+        {
+            _jogador1 = jogador1;
+            _jogador2 = jogador2;
+        }
+        public void IniciarJogo()
+        {
+           System.Console.Write(_jogador1.Chuta());
+           System.Console.Write(_jogador1.Corre());
+           System.Console.Write(_jogador1.Passe());
+            
+           System.Console.Write("\n Próximo Jogador \n");
+
+           System.Console.Write(_jogador2.Chuta());
+           System.Console.Write(_jogador2.Corre());
+           System.Console.Write(_jogador2.Passe());
+        }
+    }
+}
